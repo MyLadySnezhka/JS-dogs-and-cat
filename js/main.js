33e3ad4394000859f56b29839246c7d339c0c5a9
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-03-20 00:04:07 +0200

    add house limits

--- js/main.js
+++ js/main.js
@@ -7,19 +7,22 @@ const elCountDogs = document.querySelector('select[class=countDogs]');
 const elCountCats = document.querySelector('select[class=countCats]');
 const elBtnGo = document.querySelector('button[name=btnPush]');
 
-let X, Y;
 let catID, catX, catY;
 let countDogs, countCats;
-let cats = [{catID, catX, catY}];
+let cats = [];
 let dogs = [];
-//let house, cat, dog = [];
 let htmlDogsArr = '';
 let htmlCatsArr = '';
 let itemX, itemY;
 let houseX, houseY;
+let X, Y, xNew, yNew;
+let xLeft, yTop, xMid, yMid, xRight, yDown, xWidth, yHeight;
 
 //розмір ігрового поля
-console.log('Поле:', elPlayBoard.clientHeight, elPlayBoard.clientWidth);
+const borderX = elPlayBoard.clientWidth;
+const borderY = elPlayBoard.clientHeight;
+//const borderY = window.innerHeight;
+console.log('Поле:', borderX, borderY);
 
 const randCoord = (itemLnk) => {
     let maxX = elPlayBoard.clientWidth - itemLnk.clientWidth;
@@ -40,17 +43,16 @@ const renderItem = (itemLnk) => {
 
 const renderHouse = () => {
     randCoord(elHouse);
-    console.log('House', X, Y);
     houseX = X;
     houseY = Y;
-    elHouse.style.left = `${X}px`;
-    elHouse.style.top = `${Y}px`;
+    const _html = `<div class="house" style="left: ${houseX}px; top: ${houseY}px;"></div>`;
+    elPlayBoard.insertAdjacentHTML('beforeend', _html);
 }
 
 const renderStart = () => {
     elDog.style.left = '15%';
     elDog.style.top = '50vh';
-    elHouse.style.left = '40%';
+    elHouse.style.left = '43%';
     elHouse.style.top = '50vh';
     elCat.style.left = '75%';
     elCat.style.top = '50vh';
@@ -79,10 +81,9 @@ const renderCats = () => {
 renderStart();
 
 elCountDogs.addEventListener('change', (ev) => {
-    i = 1;
     dogs = [];
     countDogs = ev.target.value;
-    for(let i=1; i<countDogs; i=i+1) {
+    for(let i=1; i<=countDogs; i=i+1) {
         randCoord(elDog);
         let dogX = X;
         let dogY = Y;
@@ -93,39 +94,95 @@ elCountDogs.addEventListener('change', (ev) => {
 });
 
 elCountCats.addEventListener('change', (ev) => {
-    j = 1;
-    cats = [];
-    // cats[0].catX = elCat.style.left;
-    // cats[0].catY = elCat.style.top;
+    cats = [];                        
     countCats = ev.target.value;
-    for(let j=1; j<countCats; ) {
+    for(let j=1; j<=countCats; j=j+1) {
         randCoord(elCat);
         let catX = X;
         let catY = Y;
         let catID = j;
-        if ((cats[j].catX!=cats[j-1].catX)&&(cats[j].catY!=cats[j-1].catY)) {
-            cats.push({catID, catX, catY})
-            j = j+1};
+
+        cats.push({catID, catX, catY});
+        // if ((cats[j].catX=cats[j-1].catX)&&(cats[j].catY=cats[j-1].catY)) {
+        //     cats.push({catID, catX, catY});
+        //     j = j+1};
     }
     console.log('cat', cats);
 });
 
+
+
 elBtnGo.addEventListener('click', () => {
-    
-    renderHouse();
+    elHelloText.style.display = 'none';    
+    renderHouse();     
     renderCats();  
     renderDogs();
-    elHelloText.style.display = 'none';
-    
 })
 
-//альтернатива для переміщення:
-//lnkItem.style.transform = `translate(${X}px, ${Y}px)`;}
-
 elPlayBoard.addEventListener('click', (ev) => {
-    if(ev.target.classList.value != 'board') {
+
+    if((ev.target.classList.value!='board')&&(ev.target.className!='house')&&(ev.target.className!='cat')) {
+        //ширина елемента, по якому клікнули
+        //console.log(ev.target.clientWidth);
+        xLeft = parseInt(ev.target.style.left);
+        yTop = parseInt(ev.target.style.top);
+        xWidth = parseInt(ev.target.clientWidth);
+        yHeight = parseInt(ev.target.clientHeight);
+        xRight = xLeft + xWidth;
+        yDown = yTop + yHeight;
+        xMid = xLeft + xWidth/2;
+        yMid = yTop + yHeight/2;
+        console.dir(ev.target);
+        console.log('собакатыкX', xLeft, xMid, xRight);
+        console.log('собакатыкY', yTop, yMid, yDown);
+
+        let mouseX = ev.pageX;
+        let mouseY = ev.pageY;
+        console.log('мыша:', mouseX, mouseY);
         
-       console.log(ev.target.clientWidth);
+        if((mouseX>xLeft)&&(mouseX<xMid)&&(mouseY>yTop)&&(mouseY<yMid)) {
+            console.log('верхний левый угол', xLeft, xMid, yTop, yMid);
+            xNew = xLeft + xWidth/2;
+            yNew = yTop + yHeight/2;
+            console.log('новое положение', xNew, yNew);
+        } else if ((mouseX>xMid)&&(mouseX<xRight)&&(mouseY>yTop)&&(mouseY<yMid)) {
+            console.log('верхний правый угол');
+            xNew = xLeft - xWidth/2;
+            yNew = yTop + yHeight/2;
+            console.log('новое положение', xNew, yNew);
+        } else if ((mouseX>xLeft)&&(mouseX<xMid)&&(mouseY>yMid)&&(mouseY<yDown)) {
+            console.log('нижний левый угол');
+            xNew = xLeft + xWidth/2;
+            yNew = yTop - yHeight/2;
+            console.log('новое положение', xNew, yNew);
+        } else if ((mouseX>xMid)&&(mouseX<xRight)&&(mouseY>yMid)&&(mouseY<yDown)) {
+            console.log('нижний правый угол');
+            xNew = xLeft - xWidth/2;
+            yNew = yTop - yHeight/2;
+            console.log('новое положение', xNew, yNew);
+        }
+
+        //альтернатива для переміщення:
+        //lnkItem.style.transform = `translate(${X}px, ${Y}px)`;}
+
+        //перевірка, чи не вийшов за межі поля
+        if ((xNew>=0)&&(xNew<=(borderX-xWidth))&&(yNew>=0)&&(yNew<=(borderY-yHeight))) {
+            xLeft = xNew;
+            yTop = yNew;
+        } else {if(xNew<=0) {xLeft = 0};
+               if(xNew>(borderX-xWidth)) {xLeft = borderX-xWidth};
+               if(yNew<=0) {yTop = 0};
+               if(yNew>borderY-yHeight) {yTop=borderY-yHeight};
+            }  
+            ev.target.style.left = `${xLeft}px`;
+            ev.target.style.top = `${yTop}px`;
+
+            console.log('house', houseX, houseY);
+
+        //чи зайшов додому    
+        if((xLeft>=houseX-50)&&(xLeft<=houseX+80)&&(yTop>=houseY-50)&&(yTop<=houseY+120)) {
+            elPlayBoard.innerHTML = `<span class="atHome">Вітаю, цуценя вдома!<br>Якщо хочеш зіграти ще, онови сторінку.</span>`;
+        }    
     }
 });
 
